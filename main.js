000eed544d29aa14d47a578991fe41b8fe1e764a
gaurikakkr <111335758+gaurikakkr@users.noreply.github.com>
2023-10-21 21:26:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("hii")
+const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
+async function main(){
+    if(process.argv.length<3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if(process.argv.length>3){
+        console.log("too many websites provided")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+    console.log(`start crawl of : ${baseURL}`)
+    const pages = await crawlPage(baseURL, baseURL , {})
+//    for(const page of Object.entries(pages)){
+//        console.log(page)
+//    }
+    printReport(pages)
+
+
+main()
